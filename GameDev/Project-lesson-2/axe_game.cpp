272ecb5ad1aeecfd423c995ac4d2f2ac6dd1f4af
Tom Hair <tom.hair.2002@gmail.com>
2025-07-18 16:39:32 +0100

    lesson 2.4
    debugging and initializing data to get rid of garbage data

--- GameDev/Project-lesson-2/axe_game.cpp
+++ GameDev/Project-lesson-2/axe_game.cpp
@@ -6,9 +6,14 @@ Made by Tom Hair
 #include "raylib.h" //double quote external libaru
 int main()
 {
-    int width;
+    float root_beer = 1.99;// float alows for non-whole numbers, Initialization by assignment
+    double cheese_burger{5.99};//double allows for non-whole numbers with more detail takes up more memory, braced initalization
+    bool shouldhavelunch{};// bool can be either true or false and nothing else, Braced empty on bool is always falce
+    
+    int width;//creates an integer has to be whole number
     int hight;
-    hight = 400;
+    hight = 400;// defines value of integer
     width = 700;
+    
     InitWindow(width,hight,"Tom's Window"); // creates window width,hight,name
 }
