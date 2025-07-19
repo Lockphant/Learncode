4ed42fc5bb09d062d6602b0a3498763b0d8cfb45
Tom Hair Laptom <tom.hair.2002@gmail.com>
2025-07-19 02:31:51 +0100

    2.10
    used raylib to create movement for the circle and set the fps to 60

--- GameDev/Project-lesson-2/axe_game.cpp
+++ GameDev/Project-lesson-2/axe_game.cpp
@@ -7,18 +7,36 @@ Made by Tom Hair
 int main()
 {
 
-    int width;//creates an integer has to be whole number
-    int hight;
-    hight = 200;// defines value of integer
-    width = 350;
-    
+    //Window Diamentions
+    int width{350};//creates an integer has to be whole number
+    int hight{200};
     InitWindow(width,hight,"Tom's Window"); // creates window width,hight,name
 
+    //circle coordinates
+    int circle_x{175};
+    int circle_y{100};
+    int circle_r{25};
+
+    SetTargetFPS(60);
     while (WindowShouldClose() == false)//if esc or the x icon is pressed windowshouldclose swaps to false and ends the loop
     {
         BeginDrawing();
         ClearBackground(WHITE);  // makes window colour white
-        DrawCircle(175,100,25,BLUE); // draws a circle first 2 ints being co-ords 3rd being radius and colour being the colour
+
+        // game logic begins
+
+        DrawCircle(circle_x,circle_y,circle_r,BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        //game logic ends
         EndDrawing();
     }
 
